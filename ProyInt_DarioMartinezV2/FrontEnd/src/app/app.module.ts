33b78cc2278f98151733cad3fd79d06968cb3f29
darioezequielmartinez <112410778+darioezequielmartinez@users.noreply.github.com>
2022-10-08 00:53:29 -0300

    Creacion de Carpeta FrontEnd
    Se creo  Carpeta FrontEnd_ todos los contenido sdentro de la misma

--- ProyInt_DarioMartinezV2/FrontEnd/src/app/app.module.ts
+++ ProyInt_DarioMartinezV2/FrontEnd/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -13,6 +12,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -20,13 +20,13 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
     HysComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
